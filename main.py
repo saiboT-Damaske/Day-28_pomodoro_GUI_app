46b4adbeeaa011cca8d963b3807047df349fe93e
tobif <saibot01141@gmail.com>
2023-07-06 23:58:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,120 @@
+from tkinter import *
+import math
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+
+def reset_timer():
+    window.after_cancel(timer)
+    global reps
+    reps = 0
+    canvas.itemconfig(timer_text, text="00:00")
+    label_check.config(text="")
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+
+def start_timer():
+    global reps
+    work_sec = WORK_MIN * 60
+    break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # this could also be done with three case if statement using reps == 8, reps % 2 == 0 and reps % 2 != 0
+    match reps:
+        case 0:
+            count_down(work_sec)
+            reps += 1
+            label_TIMER.config(text="Work", fg=GREEN)
+        case 1:
+            count_down(break_sec)
+            reps += 1
+            label_TIMER.config(text="Break", fg=PINK)
+        case 2:
+            count_down(work_sec)
+            reps += 1
+            label_TIMER.config(text="Work", fg=GREEN)
+        case 3:
+            count_down(break_sec)
+            reps += 1
+            label_TIMER.config(text="Break", fg=PINK)
+        case 4:
+            count_down(work_sec)
+            reps += 1
+            label_TIMER.config(text="Work", fg=GREEN)
+        case 5:
+            count_down(break_sec)
+            reps += 1
+            label_TIMER.config(text="Break", fg=PINK)
+        case 6:
+            count_down(work_sec)
+            reps += 1
+            label_TIMER.config(text="Work", fg=GREEN)
+        case 7:
+            count_down(long_break_sec)
+            reps = 0
+            label_TIMER.config(text="Long Break", fg=RED)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
+
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(2, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ""
+        for i in range(math.floor(reps/2)):
+            mark += "\n✔"
+            label_check.config(text=mark)
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+photo = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=photo)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+
+label_TIMER = Label(text="Timer", fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 50, "bold"))
+label_TIMER.grid(column=1, row=0)
+
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
+start_button.grid(column=0, row=3)
+
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
+reset_button.grid(column=3, row=3)
+
+
+label_check = Label(fg=GREEN, bg=YELLOW, highlightthickness=0, font=(FONT_NAME, 20, "bold"))
+label_check.grid(column=1, row=4)
+
+
+window.mainloop()
